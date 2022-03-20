2082e8ec1d84f316ae7dc3c729174bb3d4b2cbd5
Nitesh Yadav <60145175+nteshxx@users.noreply.github.com>
2022-03-20 18:26:46 +0530

    download feature integrated

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import link from '../assets/link.svg';
 import '../styles/download.css';
+import fileDownload from 'js-file-download';
+import axios from 'axios';
 
 const Download = () => {
+  const [downloadURL, setDownloadURL] = useState(null);
+
+  const downloadFile = () => {
+    console.log('button clicked')
+    let filePath = `${downloadURL}`;
+    axios.get(`${filePath}`, {
+        responseType: 'blob',
+    }).then((res) => {
+      let filename = filePath.replace(/^.*[\\/]/, '')
+      let fileExtension;
+      fileExtension= filePath.split('.');
+      fileExtension =fileExtension[fileExtension.length -1];
+      fileDownload(res.data, `${filename}.${fileExtension}`);
+    });
+  }
+
   return (
     <div className="download">
       <div className="download-container">
         <div className="link-box">
           <img src={link} alt="" />
-          <input id="paste-link" type="text" name="download-link" placeholder="Paste download link here" />
+          <input id="paste-link" type="text" name="download-link" placeholder="Paste download link here" onChange={(e) => setDownloadURL(e.target.value)} />
         </div>
       </div>
-      <button className="download-btn">Download</button>
+      <button className="download-btn" onClick={() => downloadFile()}>Download</button>
     </div>
   )
 }
