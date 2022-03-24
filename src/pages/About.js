174735d1c0bb739d52f43fc627ab95d8de7bd644
Nitesh Yadav <60145175+nteshxx@users.noreply.github.com>
2022-03-24 10:07:45 +0530

    added about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../styles/about.css';
+import github from '../assets/github.svg';
+import linkedin from '../assets/linkedin.svg';
 
 const About = () => {
   return (
-    <h2 style={{ fontWeight: '400', opacity: '0.5', textAlign: 'center', lineHeight: '400px' }}>Work in Progress!</h2>
+    <div className="about">
+      <h2>Nitesh M. Yadav</h2>
+      <p>MERN Stack Developer</p>
+      <div className='links'>
+        <a href='https://github.com/nteshxx/file-sher-client'><img src={github} alt=''/></a>
+        <a href='https://www.linkedin.com/in/nteshxx/'><img src={linkedin} alt=''/></a>
+      </div>
+      <p>Thanks for visiting!</p>
+    </div>
   )
 }
 
-export default About
+export default About;
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -4,7 +4,10 @@ import Dropzone from '../components/Dropzone';
 
 const Upload = () => {
   return (
-    <Dropzone />
+    <div>
+      <Dropzone />
+      <p style={{ textAlign: 'right', marginRight: '10rem', marginTop: '-3rem'}}>File size must not exceed <span style={{fontWeight: 'bold'}}>100mb</span></p>
+    </div>
   )
 }
 
